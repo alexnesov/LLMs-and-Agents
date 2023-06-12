a223f95261fc43006cd642232c93ef2ff4ef926c
alexnesov <alexandrenesovic@gmail.com>
2023-06-12 16:29:44 +0200

    qa init

--- Misc/test.py
+++ Misc/test.py
@@ -1,19 +1,38 @@
-from langchain import PromptTemplate
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import LLMChain
+import os
+
+
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Pinecone
+from langchain import VectorDBQA, OpenAI
+
+import pinecone
+
+pinecone.init(api_key=os.environ.get("PINECONE_API_KEY"), environment=os.environ.get("PINECONE_ENV"))
 
 if __name__ == '__main__':
-    print('Hello LangChain')
+    print('Hello Vector Store')
+    loader = TextLoader("/home/nesov/Programmation/LLM-LangChain/Misc/Texts/NNEmbeddingsArticle.txt")
+    document = loader.load()
+    print(document)
 
-    summary_template = """
-        given the {information} about a person from I want you to create:
-        1. a short summary
-        2. two interesting facts about them
-    """
+    text_splitter = CharacterTextSplitter(chunk_size=4000, chunk_overlap=0)
+    texts = text_splitter.split_documents(document)
 
+    print(len(texts))
 
-    summary_prompt_template = PromptTemplate(input_variables="information", template=summary_template)
+    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
 
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    # take the text chunks and it will use the OpenAI embeddings API in order to converte those texts into vectors.
+    # Those vectors are going to be stored in Pinecone
+    docsearch = Pinecone.from_documents(texts, embeddings, index_name="medium-blog-embeddings-index")
 
-    chain = LLMChain 
+    # https://blog.langchain.dev/retrieval/
+
+    qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore = docsearch)
+
+    query = "Wat are Neural Network Embeddings? Give me a 15 word anwer for a beginner"
+    result = qa({"query":query})
+    
+    print(result)
