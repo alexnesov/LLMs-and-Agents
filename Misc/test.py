484b3b392ea9913921bb08a761405998c8f7bb16
alexnesov <alexandrenesovic@gmail.com>
2023-07-02 19:56:12 +0200

    working FAISS

--- Misc/test.py
+++ Misc/test.py
@@ -1,42 +1,20 @@
-import os
+#!/usr/bin/python3
 
-
-from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import Pinecone
-from langchain import VectorDBQA, OpenAI
+from langchain.vectorstores import FAISS
 
-import pinecone
-
-pinecone.init(api_key=os.environ.get("PINECONE_API_KEY"), environment=os.environ.get("PINECONE_ENV"))
+# pinecone.init(api_key=os.environ.get("PINECONE_API_KEY"), environment=os.environ.get("PINECONE_ENV"))
 
 if __name__ == '__main__':
+    # !!! The software transfer agreement document is not private, it is a publicaly available document published by the SEC !!
+    pdf_path = "Misc/Texts/Software Transfer Agreement.pdf"
+    loader = PyPDFLoader(file_path=pdf_path)
+    documents = loader.load()
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=30, separator="\n")
+    docs = text_splitter.split_documents(documents=documents)
 
-    
-    loader = TextLoader("/home/nesov/Programmation/LLM-LangChain/Misc/Texts/NNEmbeddingsArticle.txt")
-    document = loader.load()
-    print(document)
-
-    text_splitter = CharacterTextSplitter(chunk_size=4000, chunk_overlap=0)
-    texts = text_splitter.split_documents(document)
-
-    print(len(texts))
-
-    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
-
-    # take the text chunks and it will use the OpenAI embeddings API in order to convert those texts into vectors.
-    # Those vectors are going to be stored in Pinecone
-    docsearch = Pinecone.from_documents(texts, embeddings, index_name="medium-blog-embeddings-index")
-
-    # https://blog.langchain.dev/retrieval/
-
-    qa = VectorDBQA.from_chain_type(llm=OpenAI(), 
-                                    chain_type="stuff", 
-                                    vectorstore = docsearch,
-                                    return_source_answer=True) # return_source_answer=> What chunk made the LLM decide what whould be the anwser?
-
-    query = "What are Neural Network Embeddings? Give me a 15 words anwer for a beginner"
-    result = qa({"query":query})
-    
-    print(result)
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_documents(docs, embeddings)
+    vectorstore.save_local("faiss_index_react")
